1e9b5e91d210975544ae7a109cb6a823c1c820db
adadesions <adadesions@gmail.com>
2019-07-18 15:43:17 +0700

    Add caluation of histogram

--- TheKinect.cpp
+++ TheKinect.cpp
@@ -33,7 +33,7 @@ bool protonect_shutdown = false; // Whether the running application should shut 
 
 void sigint_handler(int s)
 {
-  protonect_shutdown = true;
+    protonect_shutdown = true;
 }
 
 int main()
@@ -47,7 +47,7 @@ int main()
     //! [context]
 
     //! [discovery]
-    if(freenect2.enumerateDevices() == 0)
+    if (freenect2.enumerateDevices() == 0)
     {
         std::cout << "no device connected!" << std::endl;
         return -1;
@@ -57,16 +57,18 @@ int main()
 
     std::cout << "SERIAL: " << serial << std::endl;
 
-    if(pipeline)
+    if (pipeline)
     {
         //! [open]
         dev = freenect2.openDevice(serial, pipeline);
         //! [open]
-    } else {
+    }
+    else
+    {
         dev = freenect2.openDevice(serial);
     }
 
-    if(dev == 0)
+    if (dev == 0)
     {
         std::cout << "failure opening device!" << std::endl;
         return -1;
@@ -77,8 +79,8 @@ int main()
 
     //! [listeners]
     libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color |
-                                                  libfreenect2::Frame::Depth |
-                                                  libfreenect2::Frame::Ir);
+                                                libfreenect2::Frame::Depth |
+                                                libfreenect2::Frame::Ir);
     libfreenect2::FrameMap frames;
 
     dev->setColorFrameListener(&listener);
@@ -93,28 +95,33 @@ int main()
     //! [start]
 
     //! [registration setup]
-    libfreenect2::Registration* registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
-    libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4), depth2rgb(1920, 1080 + 2, 4); // check here (https://github.com/OpenKinect/libfreenect2/issues/337) and here (https://github.com/OpenKinect/libfreenect2/issues/464) why depth2rgb image should be bigger
+    libfreenect2::Registration *registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+    libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4), depth2rgb(1920, 1080 + 2, 4);
     //! [registration setup]
 
-    Mat rgbmat, depthmat, depthmatUndistorted, irmat, rgbd, rgbd2;
+    Mat rgbmat, depthmat, depthmatUndistorted, rgbd, rgbd2;
+    int histSize = 10;
+    float range[] = {0, 1};
+    const float* histRange = { range };
+    bool uniform = true, accumulate = false;
+
+    Mat g_hist;
+    int hist_w = 512, hist_h = 300;
+    int bin_w = cvRound( (double) hist_w/histSize );
 
     //! [loop start]
-    while(!protonect_shutdown)
+    while (!protonect_shutdown)
     {
         listener.waitForNewFrame(frames);
         libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
-        libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
         libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
         //! [loop start]
 
         cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data).copyTo(rgbmat);
-        cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irmat);
         cv::Mat(depth->height, depth->width, CV_32FC1, depth->data).copyTo(depthmat);
 
-        cv::imshow("rgb", rgbmat);
-        cv::imshow("ir", irmat / 4096.0f);
-        cv::imshow("depth", depthmat / 4096.0f);
+        // cv::imshow("rgb", rgbmat);
+        // cv::imshow("depth", depthmat / 4096.0f);
 
         //! [registration]
         registration->apply(rgb, depth, &undistorted, &registered, true, &depth2rgb);
@@ -123,16 +130,27 @@ int main()
         cv::Mat(undistorted.height, undistorted.width, CV_32FC1, undistorted.data).copyTo(depthmatUndistorted);
         cv::Mat(registered.height, registered.width, CV_8UC4, registered.data).copyTo(rgbd);
         cv::Mat(depth2rgb.height, depth2rgb.width, CV_32FC1, depth2rgb.data).copyTo(rgbd2);
+        cv::Mat depthReal = depthmatUndistorted / 4500.0f;
 
-
-        cv::imshow("undistorted", depthmatUndistorted / 4096.0f);
-        cv::imshow("registered", rgbd);
-        cv::imshow("depth2RGB", rgbd2 / 4096.0f);
+        cv::imshow("undistorted", depthReal);
+        // cv::imshow("registered", rgbd);
+        // cv::imshow("depth2RGB", rgbd2 / 4096.0f);
+        calcHist(&depthReal, 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+        Mat histImage( hist_h, hist_w, CV_32FC3, Scalar( 0, 0, 0 ) );
+        normalize(g_hist, g_hist, 10, histImage.rows, NORM_MINMAX, -1, Mat() );
+        for( int i = 1; i < histSize; i++ )
+        {
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ),
+            Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+            Scalar( 0, 0, 255), 2, 8, 0  );
+        }
+        // std::cout << depthmatUndistorted/4096.0f;
+        imshow("calcHist", histImage );
 
         int key = cv::waitKey(1);
         protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
 
-    //! [loop end]
+        //! [loop end]
         listener.release(frames);
     }
     //! [loop end]
